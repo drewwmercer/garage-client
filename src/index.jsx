00b0ce1d9e729efb96e7bed8f616adef2fc9d648
Drew W Mercer <japanexpress@gmail.com>
2022-01-10 11:36:29 -0500

    Getting up and running with React

---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Import statement to indicate the need for bundling `./index.scss`
+import './index.scss';
+
+// Main component (will eventually use all the others)
+class MyGarageApplication extends React.Component {
+  render() {
+    return (
+      <div className="my-flix">
+        <div>Welcome to myGarage!</div>
+      </div>
+    );
+  }
+}
+
+// Finds the root of the app
+const container = document.getElementsByClassName('app-container')[0];
+
+// Tells React to render the app in the root DOM element
+ReactDOM.render(React.createElement(MyGarageApplication), container);
