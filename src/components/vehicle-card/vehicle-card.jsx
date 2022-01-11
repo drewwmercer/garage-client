86f6d68a7781869b890a92a5482eb9393444bf4d
Drew W Mercer <japanexpress@gmail.com>
2022-01-11 10:44:20 -0500

    Adding in prop-types

--- src/components/vehicle-card/vehicle-card.jsx
+++ src/components/vehicle-card/vehicle-card.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class VehicleCard extends React.Component {
     render() {
         const { vehicle, onVehicleClick } = this.props;
+
         return <div className="vehicle-card" onClick={() => { onVehicleClick(vehicle); }}>{vehicle.Nickname}</div>;
     }
 }
+
+VehicleCard.propTypes = {
+    // shape tells that it is an object
+    vehicle: PropTypes.shape({
+        Nickname: PropTypes.string.isRequired,
+        Year: PropTypes.string.isRequired,
+        Model: PropTypes.string.isRequired,
+        ImagePath: PropTypes.string.isRequired,
+        Make: PropTypes.shape({
+            BrandName: PropTypes.string.isRequired
+        })
+    }).isRequired,
+    onVehicleClick: PropTypes.func.isRequired
+};
